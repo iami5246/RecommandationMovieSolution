027d79645844d199a929963017eb1e4cafde466a
iami5246 <iami5246@naver.com>
2019-12-14 02:00:23 +0900

    최초수정

---
+++ MLRecommandMovieConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MLRecommandMovieConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Worlssd!1");
+        }
+    }
+}
